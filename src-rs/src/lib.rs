216d9b93f6954260e61b3a383eb8483a78609034
radcolor <radcolor@users.noreply.github.com>
2026-06-30 10:54:52 +0530

    Add Tauri single instance plugin

--- src-rs/src/lib.rs
+++ src-rs/src/lib.rs
@@ -155,6 +155,9 @@ pub fn run() {
         })
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            show_main_window(app);
+        }))
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
